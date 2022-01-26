e072b7d6921d9ee97c6113dbe470360eb44fd4b7
Dr.Rajkumar Rajasekaran <34421931+vitrajkumar@users.noreply.github.com>
2022-01-26 12:32:06 +0530

    Text Analytics - Word cloud
    word cloud - Dr Abdul kalam speech 2007 republic day

---
+++ Wordcloud.R
@@ -0,0 +1,54 @@
+# Install
+install.packages("tm")  # for text mining
+install.packages("SnowballC") # for text stemming
+install.packages("wordcloud") # word-cloud generator 
+install.packages("RColorBrewer") # color palettes
+# Load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+## Read the text file from internet
+
+# text <- readLines(filePath)
+text <- readLines(file.choose())
+# Load the data as a corpus
+docs <- Corpus(VectorSource(text))
+inspect(docs)
+# Text transformation,Transformation is performed using tm_map() function to replace, for example, special characters from the text.
+# Replacing "/", "@" and "|" with space:
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+# Cleaning the Text
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Remove your own stop word
+# specify your stopwords as a character vector
+docs <- tm_map(docs, removeWords, c("Abdul", "kalam")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+# docs <- tm_map(docs, stemDocument)
+# Build a term-document matrix (table containing the frequency of the words)
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+findFreqTerms(dtm, lowfreq = 4)
+head(d, 20)
+barplot(d[1:10,]$freq, las = 2, names.arg = d[1:10,]$word,
+        col ="lightblue", main ="Most frequent words",
+        ylab = "Word frequencies")
